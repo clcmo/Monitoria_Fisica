0a3daad8459a69fa5fb868763cd10d0fec77a446
Camila Leite (Milla) <ola@camilaloliveira.com>
2026-09-03 12:24:02 +0000

    Ajusta base path para ./ no vite.config.ts e garante .nojekyll

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs.plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './', // Força os arquivos JS/CSS a serem buscados via caminho relativo
 })
